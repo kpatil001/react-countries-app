787a349fa6af89fde4485497394df666a7aba7f6
Krunal Patil <pkrunalb888@gmail.com>
2020-08-15 17:35:33 +0530

    Updates

---
+++ precache-manifest.4aab198a9b55a12a99f53db57fa9c60e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ffcbb825cd274e0535dfc7b9e51fcb",
+    "url": "/react-countries-app/index.html"
+  },
+  {
+    "revision": "ee1d8d4e16fba3ad36b6",
+    "url": "/react-countries-app/static/css/main.118056cf.chunk.css"
+  },
+  {
+    "revision": "3fa6c568769823d14ba5",
+    "url": "/react-countries-app/static/js/2.65ece292.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-countries-app/static/js/2.65ece292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1d8d4e16fba3ad36b6",
+    "url": "/react-countries-app/static/js/main.613cad0d.chunk.js"
+  },
+  {
+    "revision": "d8816e72bd224c9c5dd8",
+    "url": "/react-countries-app/static/js/runtime-main.ea4cf73b.js"
+  }
+]);
